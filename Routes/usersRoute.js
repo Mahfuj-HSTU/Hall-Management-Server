83d130ce5e9167bfc1471016690fdf73e2849e81
mahfuj <rmahfujur@gmail.com>
2024-02-10 18:39:34 +0600

    delete user

--- Routes/usersRoute.js
+++ Routes/usersRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.route('/').get(usersController.getUsers).post(usersController.postUsers);
 router.route('/:email').get(usersController.getUser);
+router.route('/:id').delete(usersController.deleteUser);
 
 module.exports = router;
--- controller/usersController.js
+++ controller/usersController.js
@@ -1,7 +1,11 @@
 const { query } = require('express');
 const dbConnect = require('../Utilities/dbConnect');
+const { ObjectId } = require('mongodb');
 
 const userCollection = dbConnect().db('HallManagement').collection('users');
+const studentCollection = dbConnect()
+  .db('HallManagement')
+  .collection('students');
 
 const getUsers = async (req, res) => {
   const query = {};
@@ -34,4 +38,62 @@ const getUser = async (req, res) => {
   res.send(user);
 };
 
-module.exports = { getUsers, postUsers, getUser };
+const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const query = { sid: id };
+  // console.log(query);
+  try {
+    // Delete user from usersCollection
+    const userDelete = await userCollection.deleteOne(query);
+
+    // Delete user from studentCollection
+    const studentDelete = await studentCollection.deleteMany(query);
+
+    if (userDelete.deletedCount === 1 && studentDelete.deletedCount >= 1) {
+      res
+        .status(200)
+        .send({ message: 'User deleted successfully from both collections.' });
+    } else {
+      res.status(404).send({
+        message:
+          'User not found or could not be deleted from both collections.',
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ message: 'Error deleting user.', error: error });
+  }
+};
+
+// app.delete('/users/:id', async (req, res) => {
+//   const id = req.params.id;
+//   const userIdQuery = { _id: ObjectID(id) };
+//   const studentIdQuery = { userId: id }; // Assuming there's a field named 'userId' in the studentCollection referencing the user's ID
+
+//   try {
+//     // Delete user from usersCollection
+//     const userDeletionResult = await usersCollection.deleteOne(userIdQuery);
+
+//     // Delete user from studentCollection
+//     const studentDeletionResult = await studentCollection.deleteMany(
+//       studentIdQuery
+//     );
+
+//     if (
+//       userDeletionResult.deletedCount === 1 &&
+//       studentDeletionResult.deletedCount >= 1
+//     ) {
+//       res
+//         .status(200)
+//         .send({ message: 'User deleted successfully from both collections.' });
+//     } else {
+//       res.status(404).send({
+//         message:
+//           'User not found or could not be deleted from both collections.',
+//       });
+//     }
+//   } catch (error) {
+//     res.status(500).send({ message: 'Error deleting user.', error: error });
+//   }
+// });
+
+module.exports = { getUsers, postUsers, getUser, deleteUser };
