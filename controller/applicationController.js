54cf82d4585c34e4ffba46d7a4f7d854385d57bc
mahfuj <rmahfujur@gmail.com>
2024-02-12 23:38:07 +0600

    update applications

--- controller/applicationController.js
+++ controller/applicationController.js
@@ -16,25 +16,28 @@ const addApplication = async (req, res) => {
   try {
     const data = req.body;
     const sid = data.sid;
-    const type = data.type;
+    const type = data?.type;
+    const currentDate = new Date();
+    delete data?._id;
+    // console.log(data);
 
     const existingApplications = await applicationCollection
       .find({ sid })
       .toArray();
+    console.log(data);
 
+    const [firstApplication, secondApplication] = existingApplications;
     if (existingApplications.length === 2) {
-      const [firstApplication, secondApplication] = existingApplications;
-
       if (type === firstApplication.type) {
         // Update the first application
         const filter = { _id: firstApplication._id };
-        const updateDoc = { $set: data };
+        const updateDoc = { $set: { ...data, date: currentDate } };
         await applicationCollection.updateOne(filter, updateDoc);
         return res.json({ message: 'Application updated.' });
       } else if (type === secondApplication.type) {
         // Update the second application
         const filter = { _id: secondApplication._id };
-        const updateDoc = { $set: data };
+        const updateDoc = { $set: { ...data, date: currentDate } };
         await applicationCollection.updateOne(filter, updateDoc);
         return res.json({ message: 'Application updated.' });
       } else {
@@ -43,8 +46,21 @@ const addApplication = async (req, res) => {
             'Cannot update application. New type does not match existing types.',
         });
       }
+    } else if (
+      type === firstApplication?.type ||
+      type === secondApplication?.type
+    ) {
+      // Update the second application
+      const filter = { _id: firstApplication?._id || secondApplication?._id };
+      // console.log(filter);
+      const updateDoc = { $set: { ...data, date: currentDate } };
+      await applicationCollection.updateOne(filter, updateDoc);
+      return res.json({ message: 'Application updated.' });
     } else {
-      const result = await applicationCollection.insertOne(data);
+      const result = await applicationCollection.insertOne({
+        ...data,
+        date: currentDate,
+      });
       return res.send(result);
     }
   } catch (error) {
