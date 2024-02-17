3cd925004f3924ba2fec8c7246b6709a1a12faa3
mahfuj <rmahfujur@gmail.com>
2024-02-18 01:47:06 +0600

    update noitce api

--- controller/noticeController.js
+++ controller/noticeController.js
@@ -9,10 +9,36 @@ const getNotice = async (req, res) => {
   const notices = await cursor.toArray();
   res.send(notices);
 };
+
 const addNotice = async (req, res) => {
   const data = req.body;
-  const notice = await noticeCollection.insertOne(data);
-  res.send(notice);
+  const type = data.type;
+
+  try {
+    let existingNotice;
+
+    if (type === 'HallSeat') {
+      // If type is 'HallSeat', try to find an existing notice
+      existingNotice = await noticeCollection.findOneAndUpdate(
+        { type },
+        { $set: data },
+        {
+          upsert: true, // Insert if not found
+          returnOriginal: false, // Return the updated or inserted document
+        }
+      );
+    } else {
+      // If type is not 'HallSeat', directly insert a new notice
+      existingNotice = await noticeCollection.insertOne(data);
+    }
+
+    // console.log(existingNotice);
+
+    res.status(200).send(existingNotice);
+  } catch (error) {
+    console.error('Error adding notice:', error);
+    res.status(500).send('Error adding notice');
+  }
 };
 
 module.exports = {
